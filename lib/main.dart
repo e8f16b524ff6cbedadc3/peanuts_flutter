dc18407b705c74147b574f298d746b20f2a10847
Owen Chia <aptx945@gmail.com>
2019-06-07 19:43:36 +0800

    add draggable scrollbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'dart:async';
-import 'dart:convert';
-
+import 'package:draggable_scrollbar/draggable_scrollbar.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 import 'package:gbk_codec/gbk_codec.dart';
+import 'package:html/dom.dart' as dom;
 import 'package:html/parser.dart' show parse;
+import 'package:http/http.dart';
 
 Future<List<String>> fetchPrices(Client client) async {
-  Response resp = await client.get('http://www.62422.cn/look.asp?id=372975');
+  Response resp = await client.get('http://www.62422.cn/search.asp?cataid=77');
   var document = parse(gbk_bytes.decode(resp.bodyBytes));
+  List<dom.Element> links = document.querySelectorAll('a[href^=look]');
+  String urlPath = links.where((it) => it.text.contains('山东')).first.attributes['href'];
+
+  //String urlPath = "look.asp?id=372975";
+  resp = await client.get('http://www.62422.cn/${urlPath}');
+  document = parse(gbk_bytes.decode(resp.bodyBytes));
   String title = document.querySelector('title').text.split(':')[0];
   String location = title.split('地区')[0].split('日')[1];
   List<String> data = document.body.text
@@ -24,31 +30,37 @@ Future<List<String>> fetchPrices(Client client) async {
 
 class PricesList extends StatelessWidget {
   final List<String> prices;
+  final ScrollController controller = ScrollController();
 
   PricesList({Key key, this.prices}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: prices.length,
-      itemBuilder: (BuildContext ctx, int index) {
-        if (index == 0) {
+    return DraggableScrollbar.rrect(
+    //return Scrollbar(
+      controller: controller,
+      child: ListView.builder(
+        controller: controller,
+        itemCount: prices.length,
+        itemBuilder: (BuildContext ctx, int index) {
+          if (index == 0) {
+            return Card(
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  prices[index],
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            );
+          }
           return Card(
             child: Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Text(
-                prices[index],
-                style: TextStyle(fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-            ),
+                padding: EdgeInsets.all(10.0), child: Text(prices[index])),
           );
-        }
-        return Card(
-          child: Padding(
-              padding: EdgeInsets.all(10.0), child: Text(prices[index])),
-        );
-      },
+        },
+      ),
     );
   }
 }
